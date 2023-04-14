60fa430d9d5259af62171a78ac73c8c4a737d5e9
Ewesterol <ewesterol2@gmail.com>
2023-04-14 17:36:40 +0200

    atrr fixed

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 let randomNum1 = Math.floor(Math.random() * 6) + 1;
 let randomNum2 = Math.floor(Math.random() * 6) + 1;
 let randomSrc1 = "images/dice" + randomNum1 + ".png";
-$("img").attr("src", randomSrc1);
 let randomSrc2 = "images/dice" + randomNum2 + ".png";
-$("img").attr("src", randomSrc2);
+$(".img1").attr("src", randomSrc1);
+
+$(".img2").attr("src", randomSrc2);
 
 if (randomNum1 > randomNum2) {
   $("h1").innerHTML = "player 1 won🎉!";
